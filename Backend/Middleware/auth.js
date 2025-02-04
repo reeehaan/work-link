a127a52e937e0bcf2d1af7f35881710d5900d159
IamAjhere <Ajcreationsajmal@gmail.com>
2025-02-04 16:24:03 +0530

    patch: client profile

--- Backend/Middleware/auth.js
+++ Backend/Middleware/auth.js
@@ -1,35 +1,35 @@
 const jwt = require('jsonwebtoken');
 
-// Verify JWT token middleware 
+// Verify JWT token middleware
 const verifyToken = (req, res, next) => {
-    const authHeader = req.header('Authorization');
-    if (!authHeader) return res.status(401).send('Access Denied');
+  const authHeader = req.header('Authorization');
+  if (!authHeader) return res.status(401).send('Access Denied');
 
-    // Remove 'Bearer ' from token string
-    const token = authHeader.split(' ')[1];
-    
-    try {
-        const verified = jwt.verify(token, process.env.TOKEN_SECRET);
-        req.user = verified;
-        next();
-    } catch (err) {
-        res.status(401).send('Invalid Token');
-    }
+  // Remove 'Bearer ' from token string
+  const token = authHeader.split(' ')[1];
+
+  try {
+    const verified = jwt.verify(token, process.env.TOKEN_SECRET);
+    req.user = verified;
+    next();
+  } catch (err) {
+    res.status(401).send('Invalid Token');
+  }
 };
 
 // Verify client role middleware
 const verifyClientRole = (req, res, next) => {
-    if (req.user.role !== 'client') {
-        return res.status(403).send('Access Denied: Client role required');
-    }
-    next();
+  if (req.user.role !== 'client') {
+    return res.status(403).send('Access Denied: Client role required');
+  }
+  next();
 };
 
 const verifyFreelancerRole = (req, res, next) => {
-    if (req.user.role !== 'freelancer') {
-        return res.status(403).send('Access Denied: Freelancer role required');
-    }
-    next();
+  if (req.user.role !== 'freelancer') {
+    return res.status(403).send('Access Denied: Freelancer role required');
+  }
+  next();
 };
 
 module.exports = { verifyToken, verifyClientRole, verifyFreelancerRole };
--- Frontend/src/Components/Navbar/Navbar.jsx
+++ Frontend/src/Components/Navbar/Navbar.jsx
@@ -1,16 +1,24 @@
-import { useState, useEffect} from 'react';
-import { AppBar, Toolbar, Button, TextField, Box, Typography, Menu, MenuItem, Avatar, IconButton } from '@mui/material';
+import { useState, useEffect } from 'react';
+import {
+  AppBar,
+  Toolbar,
+  Button,
+  TextField,
+  Box,
+  Typography,
+  Menu,
+  MenuItem,
+  Avatar,
+  IconButton,
+} from '@mui/material';
 import { ArrowDropDown } from '@mui/icons-material';
 import { useNavigate } from 'react-router-dom';
 import PropTypes from 'prop-types';
 import { jwtDecode } from 'jwt-decode';
-import axios from "axios";
-
+import axios from 'axios';
 
 export default function Navbar({ userType }) {
   const navigate = useNavigate();
-  
-
 
   const [anchorElWork, setAnchorElWork] = useState(null);
   const [anchorElDeliver, setAnchorElDeliver] = useState(null);
@@ -37,7 +45,12 @@ export default function Navbar({ userType }) {
   const freelancerLinks = () => (
     <Box sx={{ display: 'flex', gap: 2 }}>
       <div>
-        <Button color="inherit" onClick={handleMenuClick(setAnchorElWork)} endIcon={<ArrowDropDown />} sx={{ color: '#007bff' }}>
+        <Button
+          color='inherit'
+          onClick={handleMenuClick(setAnchorElWork)}
+          endIcon={<ArrowDropDown />}
+          sx={{ color: '#007bff' }}
+        >
           Find Work
         </Button>
         <Menu
@@ -47,14 +60,28 @@ export default function Navbar({ userType }) {
           anchorOrigin={{ vertical: 'bottom', horizontal: 'left' }}
           transformOrigin={{ vertical: 'top', horizontal: 'left' }}
         >
-          <MenuItem onClick={() => handleFindWorkClick()} href="/project/all">Find Work</MenuItem>
-          <MenuItem onClick={() => handleSaveProjectClick()} href="/savedJobs">Saved Projects</MenuItem>
-          <MenuItem onClick={handleMenuClose(setAnchorElWork)} href="/proposals">Proposals</MenuItem>
+          <MenuItem onClick={() => handleFindWorkClick()} href='/project/all'>
+            Find Work
+          </MenuItem>
+          <MenuItem onClick={() => handleSaveProjectClick()} href='/savedJobs'>
+            Saved Projects
+          </MenuItem>
+          <MenuItem
+            onClick={handleMenuClose(setAnchorElWork)}
+            href='/proposals'
+          >
+            Proposals
+          </MenuItem>
         </Menu>
       </div>
 
       <div>
-        <Button color="inherit" onClick={handleMenuClick(setAnchorElDeliver)} endIcon={<ArrowDropDown />} sx={{ color: '#007bff' }}>
+        <Button
+          color='inherit'
+          onClick={handleMenuClick(setAnchorElDeliver)}
+          endIcon={<ArrowDropDown />}
+          sx={{ color: '#007bff' }}
+        >
           Deliver Work
         </Button>
         <Menu
@@ -64,19 +91,37 @@ export default function Navbar({ userType }) {
           anchorOrigin={{ vertical: 'bottom', horizontal: 'left' }}
           transformOrigin={{ vertical: 'top', horizontal: 'left' }}
         >
-          <MenuItem onClick={handleMenuClose(setAnchorElDeliver)} href="/activeContracts">Active Contracts</MenuItem>
+          <MenuItem
+            onClick={handleMenuClose(setAnchorElDeliver)}
+            href='/activeContracts'
+          >
+            Active Contracts
+          </MenuItem>
         </Menu>
       </div>
 
-      <Button color="inherit" href="/refund" sx={{ color: '#007bff' }}>Refund Policy</Button>
-      <Button color="inherit" onClick={() => handleMessageClick()} sx={{ color: '#007bff' }}>Messages</Button>
+      <Button color='inherit' href='/refund' sx={{ color: '#007bff' }}>
+        Refund Policy
+      </Button>
+      <Button
+        color='inherit'
+        onClick={() => handleMessageClick()}
+        sx={{ color: '#007bff' }}
+      >
+        Messages
+      </Button>
     </Box>
   );
 
   const clientLinks = () => (
     <Box sx={{ display: 'flex', gap: 2 }}>
       <div>
-        <Button color="inherit" onClick={handleMenuClick(setAnchorElPostJob)} endIcon={<ArrowDropDown />} sx={{ color: '#007bff' }}>
+        <Button
+          color='inherit'
+          onClick={handleMenuClick(setAnchorElPostJob)}
+          endIcon={<ArrowDropDown />}
+          sx={{ color: '#007bff' }}
+        >
           Post Job
         </Button>
         <Menu
@@ -86,13 +131,25 @@ export default function Navbar({ userType }) {
           anchorOrigin={{ vertical: 'bottom', horizontal: 'left' }}
           transformOrigin={{ vertical: 'top', horizontal: 'left' }}
         >
-          <MenuItem onClick={handlePostProject} href="/post-project-layout" >Post a Project</MenuItem>
-          <MenuItem onClick={handleMenuClose(setAnchorElPostJob)} href="/dashboard">Dashboard</MenuItem>
+          <MenuItem onClick={handlePostProject} href='/post-project-layout'>
+            Post a Project
+          </MenuItem>
+          <MenuItem
+            onClick={handleMenuClose(setAnchorElPostJob)}
+            href='/dashboard'
+          >
+            Dashboard
+          </MenuItem>
         </Menu>
       </div>
 
       <div>
-        <Button color="inherit" onClick={handleMenuClick(setAnchorElHires)} endIcon={<ArrowDropDown />} sx={{ color: '#007bff' }}>
+        <Button
+          color='inherit'
+          onClick={handleMenuClick(setAnchorElHires)}
+          endIcon={<ArrowDropDown />}
+          sx={{ color: '#007bff' }}
+        >
           Your Hires
         </Button>
         <Menu
@@ -102,12 +159,22 @@ export default function Navbar({ userType }) {
           anchorOrigin={{ vertical: 'bottom', horizontal: 'left' }}
           transformOrigin={{ vertical: 'top', horizontal: 'left' }}
         >
-          <MenuItem onClick={handleMenuClose(setAnchorElHires)} href="/yourhires">Your Hires</MenuItem>
+          <MenuItem
+            onClick={handleMenuClose(setAnchorElHires)}
+            href='/yourhires'
+          >
+            Your Hires
+          </MenuItem>
         </Menu>
       </div>
 
       <div>
-        <Button color="inherit" onClick={handleMenuClick(setAnchorElReport)} endIcon={<ArrowDropDown />} sx={{ color: '#007bff' }}>
+        <Button
+          color='inherit'
+          onClick={handleMenuClick(setAnchorElReport)}
+          endIcon={<ArrowDropDown />}
+          sx={{ color: '#007bff' }}
+        >
           Report
         </Button>
         <Menu
@@ -117,119 +184,158 @@ export default function Navbar({ userType }) {
           anchorOrigin={{ vertical: 'bottom', horizontal: 'left' }}
           transformOrigin={{ vertical: 'top', horizontal: 'left' }}
         >
-          <MenuItem onClick={handleMenuClose(setAnchorElReport)} href="/weeklySummary">Weekly Summary</MenuItem>
-          <MenuItem onClick={handleMenuClose(setAnchorElReport)} href="/transactionHistory">Transaction History</MenuItem>
+          <MenuItem
+            onClick={handleMenuClose(setAnchorElReport)}
+            href='/weeklySummary'
+          >
+            Weekly Summary
+          </MenuItem>
+          <MenuItem
+            onClick={handleMenuClose(setAnchorElReport)}
+            href='/transactionHistory'
+          >
+            Transaction History
+          </MenuItem>
         </Menu>
       </div>
 
-      <Button color="inherit" onClick={() => handleMessageClick()}  sx={{ color: '#007bff' }}>Messages</Button>
+      <Button
+        color='inherit'
+        onClick={() => handleMessageClick()}
+        sx={{ color: '#007bff' }}
+      >
+        Messages
+      </Button>
     </Box>
   );
-//logout 
+  //logout
   const handleLogout = () => {
     localStorage.removeItem('accessToken');
     navigate('/');
   };
 
-  const handlePostProject = () =>{
+  const handlePostProject = () => {
     navigate('/post-project-layout');
     setAnchorElPostJob(null);
-  }
+  };
 
-  const handleMessageClick = () =>{
+  const handleMessageClick = () => {
     navigate('/live-chat');
     setAnchorElPostJob(null);
-  }
+  };
 
-  const handleSaveProjectClick = () =>{
+  const handleSaveProjectClick = () => {
     navigate('/freelancer/save-project');
     setAnchorElPostJob(null);
-  }
+  };
 
-  const handleProfileClick = () =>{
-    navigate('/freelancer/profile');
+  const handleProfileClick = () => {
+    userType === 'freelancer'
+      ? navigate('/freelancer/profile')
+      : navigate('/client/profile');
     setAnchorElPostJob(null);
-  }
-  
+  };
 
   const handleFindWorkClick = () => {
     navigate('/freelancer');
-    setAnchorElPostJob(null); 
+    setAnchorElPostJob(null);
   };
 
-    //To display user's name in the navbar
-    const [userProfile, setUserProfile] = useState({ firstName: '', lastName: '' });
+  //To display user's name in the navbar
+  const [userProfile, setUserProfile] = useState({
+    firstName: '',
+    lastName: '',
+  });
 
-
-    const fetchUserProfileName = async () => {
-      try {
-        const token = localStorage.getItem('accessToken'); // Assuming JWT is stored in localStorage as 'accessToken'
-        if (token) {
-          const decodedToken = jwtDecode(token);
-          const userId = decodedToken._id; // Adjust this depending on the structure of your JWT payload
-          const response = await axios.get(`http://localhost:3000/api/user/${userId}`);
-          console.log(response);
-          const { firstName, lastName, image, } = response.data;
-          setUserProfile({ firstName, lastName, image }); // Update the userProfile state with the fetched data
-          // Call the function to handle profile cli
-        } else {
-          console.error('Token not found in localStorage');
-        }
-      } catch (error) {
-        console.error('Failed to fetch user profile:', error);
+  const fetchUserProfileName = async () => {
+    try {
+      const token = localStorage.getItem('accessToken'); // Assuming JWT is stored in localStorage as 'accessToken'
+      if (token) {
+        const decodedToken = jwtDecode(token);
+        const userId = decodedToken._id; // Adjust this depending on the structure of your JWT payload
+        const response = await axios.get(
+          `http://localhost:3000/api/user/${userId}`
+        );
+        console.log(response);
+        const { firstName, lastName, image } = response.data;
+        setUserProfile({ firstName, lastName, image }); // Update the userProfile state with the fetched data
+        // Call the function to handle profile cli
+      } else {
+        console.error('Token not found in localStorage');
       }
-    };
-    
-    useEffect(() => {
-      fetchUserProfileName();
-    }, []);
-    
-  
+    } catch (error) {
+      console.error('Failed to fetch user profile:', error);
+    }
+  };
 
-  
+  useEffect(() => {
+    fetchUserProfileName();
+  }, []);
+
   return (
-      <AppBar position="sticky" sx={{ backgroundColor: '#ffffff', color: '#000000' }}>
-        <Toolbar sx={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center' }}>
-          <Typography variant="h6" sx={{ flexGrow: 1 }}>
-            <a href="/" style={{ textDecoration: 'none', color: '#000000', fontWeight: 'bold' }}>
-              Work<span style={{ color: 'rgb(0, 123, 255)' }}>Link</span>
-            </a>
-          </Typography>
-  
-          {userType === 'freelancer' ? freelancerLinks() : userType === 'client' ? clientLinks() : null}
-  
-          <TextField
-            variant="outlined"
-            size="small"
-            sx={{ backgroundColor: 'white', borderRadius: '4px', marginRight: 2 }}
-            placeholder="Search"
-          />
+    <AppBar
+      position='sticky'
+      sx={{ backgroundColor: '#ffffff', color: '#000000' }}
+    >
+      <Toolbar
+        sx={{
+          display: 'flex',
+          justifyContent: 'space-between',
+          alignItems: 'center',
+        }}
+      >
+        <Typography variant='h6' sx={{ flexGrow: 1 }}>
+          <a
+            href='/'
+            style={{
+              textDecoration: 'none',
+              color: '#000000',
+              fontWeight: 'bold',
+            }}
+          >
+            Work<span style={{ color: 'rgb(0, 123, 255)' }}>Link</span>
+          </a>
+        </Typography>
 
-          <Typography variant="body1" sx={{ marginRight: 2 , fontWeight:400}}>
-            {userProfile.firstName} {userProfile.lastName}
-          </Typography>
-         
-          <div>
-            <IconButton onClick={handleMenuClick(setAnchorElProfile)}>
-              <Avatar alt="" src={userProfile.image} />
-            </IconButton>
-            <Menu
-              anchorEl={anchorElProfile}
-              open={openProfile}
-              onClose={handleMenuClose(setAnchorElProfile)}
-              anchorOrigin={{ vertical: 'bottom', horizontal: 'right' }}
-              transformOrigin={{ vertical: 'top', horizontal: 'right' }}
-            >
-              <MenuItem onClick={() => handleProfileClick()} href="/">Profile</MenuItem>
-              <MenuItem onClick={() => handleLogout()}>Logout</MenuItem>
-            </Menu>
-          </div>
-        </Toolbar>
-      </AppBar>
-    );
+        {userType === 'freelancer'
+          ? freelancerLinks()
+          : userType === 'client'
+          ? clientLinks()
+          : null}
+
+        <TextField
+          variant='outlined'
+          size='small'
+          sx={{ backgroundColor: 'white', borderRadius: '4px', marginRight: 2 }}
+          placeholder='Search'
+        />
+
+        <Typography variant='body1' sx={{ marginRight: 2, fontWeight: 400 }}>
+          {userProfile.firstName} {userProfile.lastName}
+        </Typography>
+
+        <div>
+          <IconButton onClick={handleMenuClick(setAnchorElProfile)}>
+            <Avatar alt='' src={userProfile.image} />
+          </IconButton>
+          <Menu
+            anchorEl={anchorElProfile}
+            open={openProfile}
+            onClose={handleMenuClose(setAnchorElProfile)}
+            anchorOrigin={{ vertical: 'bottom', horizontal: 'right' }}
+            transformOrigin={{ vertical: 'top', horizontal: 'right' }}
+          >
+            <MenuItem onClick={() => handleProfileClick()} href='/'>
+              Profile
+            </MenuItem>
+            <MenuItem onClick={() => handleLogout()}>Logout</MenuItem>
+          </Menu>
+        </div>
+      </Toolbar>
+    </AppBar>
+  );
 }
 
 Navbar.propTypes = {
   userType: PropTypes.oneOf(['freelancer', 'client']).isRequired,
 };
-
